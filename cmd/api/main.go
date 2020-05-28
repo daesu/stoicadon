f05071397aaffbe32ae0712f922540f4fda65d0c
Paul Ryan <pol.oriain@gmail.com>
2020-05-28 13:54:44 +0900

    fix linter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,10 +8,10 @@ import (
 func main() {
 	application, err := api.ConfigureApplication()
 	if err != nil {
-		logrus.Fatalf("error: %w", err)
+		logrus.Fatalf("error: %s", err.Error())
 	}
 
 	if err := api.StartAPI(application); err != nil {
-		logrus.Fatalf("could not start api: %w", err)
+		logrus.Fatalf("could not start api: %s", err.Error())
 	}
 }
